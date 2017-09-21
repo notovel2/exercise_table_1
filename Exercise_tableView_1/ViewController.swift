48f5869eda497044eff43e4fa6807c2808de6920
notovel2 <notovel2@gmail.com>
2017-09-21 11:40:35 +0700

    TableView

---
+++ Exercise_tableView_1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Exercise_tableView_1
+//
+//  Created by 6272 on 9/21/2560 BE.
+//  Copyright © 2560 6272. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var pname: String = ""
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = pname
+        imageView.image = UIImage(named: pname+".jpeg")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
